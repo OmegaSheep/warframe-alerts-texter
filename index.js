69087ad2ff6ac45a3edb8120b52f67451b3043c1
SuperSheep18 <nothingwillgowrong@gmail.com>
2018-04-08 19:18:31 -0600

    Socket.io passing data to frontend.

--- index.js
+++ index.js
@@ -52,7 +52,9 @@ app.get('/', function(request, response) {
   });
 });
 
-server.listen(app.get('port'));
+server.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
 /*
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
