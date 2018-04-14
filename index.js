be182911f9ab39ed4070a599baf971e8128048b0
SuperSheep18 <nothingwillgowrong@gmail.com>
2018-04-13 23:08:07 -0600

    Favicon change.

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ var Item = mongoose.model('Item', SCHEMA.itemSchema);
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(__dirname + '/public/images/favicon_wta.ico'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
